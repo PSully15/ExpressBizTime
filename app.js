2b0b5ea27c2b7625f6844aab216328ca2b7ba0b7
Patrick Sullivan <patricksullivan@patricks-air.myfiosgateway.com>
2023-06-17 00:09:42 -0400

    initiated npm, added db.js, server.js and exressError.js

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-/** BizTime express application. */
-
-
-const express = require("express");
+const express = require('express');
+const ExpressError = require('./expressError');
+const morgan = require('morgan');
 
 const app = express();
-const ExpressError = require("./expressError")
 
+// parse request bodies for JSON
 app.use(express.json());
+app.use(morgan('dev'));
 
-
-/** 404 handler */
-
-app.use(function(req, res, next) {
-  const err = new ExpressError("Not Found", 404);
-  return next(err);
+// 404 handler
+app.use((req, res, next) => {
+	const e = new ExpressError('Page Not Found', 404);
+	// pass the error to the next piece of middleware
+	return next(e);
 });
 
-/** general error handler */
-
+// general error handler
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
+	// the default status is 500 Internal Server Error
+	let status = err.status || 500;
+	let message = err.msg;
 
-  return res.json({
-    error: err,
-    message: err.message
-  });
+	// set the status and alert the user
+	return res.status(status).json({
+		error: {
+			message: message,
+			status: status,
+		},
+	});
 });
-
 
 module.exports = app;
