26d762322c3aa14fab86fee1cfb7ef20dc14151f
Patrick Sullivan <patricksullivan@patricks-air.myfiosgateway.com>
2023-06-17 00:31:46 -0400

    fixed db and data.sql

--- db.js
+++ db.js
@@ -1,11 +1,19 @@
-/** Database setup for BizTime. */
-
 const { Client } = require('pg');
 
-const client = new Client({
-	connectionString: 'postgresql:///biztime',
+let DB_URI;
+
+// If we're running in test "mode", use our test db
+if (process.env.NODE_ENV === 'test') {
+	DB_URI = 'postgresql:///biztime_test';
+} else {
+	DB_URI = 'postgresql:///biztime';
+}
+
+// create the client to connect to the db
+let db = new Client({
+	connectionString: DB_URI,
 });
 
-client.connect();
+db.connect();
 
-module.exports = client;
+module.exports = db;
